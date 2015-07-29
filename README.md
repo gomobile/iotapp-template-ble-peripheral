a097b93244c85b1561c17bfd5ffd63c82dcc6b18
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-07-29 16:33:20 -0700

    add article URL and template description

--- README.md
+++ README.md
@@ -153,7 +153,7 @@ Intel(R) XDK IoT Edition
 -------------------------------------------
 This template is part of the Intel(R) XDK IoT Edition. 
 Download the Intel(R) XDK IoT Edition at https://software.intel.com/en-us/html5/xdk-iot. To see the technical details of the sample, 
-please visit the sample article page at TBA.
+please visit the sample article page at https://software.intel.com/en-us/creating-a-bluetooth-low-energy-app/.
 
 
 Important App Files
--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+/*jslint node:true,vars:true,bitwise:true,unparam:true */
+
+/*jshint unused:true */
+
+/*
+The BLE - Peripheral Node.js sample application distributed within Intel® XDK IoT Edition under the IoT with Node.js Projects project creation option showcases how to advertise it's presence, read and write data via it's service and corresponding characteristic for Bluetooth Low Energy (BLE) communication.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing/updating MRAA & UPM Library on Intel IoT Platforms with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+OR
+In Intel XDK IoT Edition under the Develop Tab (for Internet of Things Embedded Application)
+Develop Tab
+1. Connect to board via the IoT Device Drop down (Add Manual Connection or pick device in list)
+2. Press the "Settings" button
+3. Click the "Update libraries on board" option
+
+Review README.md file for more information about enabling bluetooth and completing the desired configurations.
+
+Article: https://software.intel.com/en-us/creating-a-bluetooth-low-energy-app/ 
+*/
+
 var bleno = require('bleno');
 
 var BlenoPrimaryService = bleno.PrimaryService;
